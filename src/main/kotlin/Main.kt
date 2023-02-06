2c37b44ca84d9389bd22fbc5026bcfac31ff4dbb
Vadim Gritsko <vadim.gritsko@gmail.com>
2023-02-06 12:51:20 +0700

    1470. Shuffle the Array

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+import easy.Solution
+
+fun main(args: Array<String>) {
+
+    val inputs = intArrayOf(1,2,3,4,4,3,2,1)
+    val res = Solution().shuffle(inputs, 4)
+}
---
+++ src/main/kotlin/easy/ShuffleTheArray.kt
@@ -0,0 +1,16 @@
+package easy
+
+//1470. Shuffle the Array
+//https://leetcode.com/problems/shuffle-the-array/
+class Solution {
+    fun shuffle(nums: IntArray, n: Int): IntArray {
+        val newArray = IntArray(nums.size)
+        var j = 0
+        for (i in 0 until n) {
+            newArray[j] = nums[i]
+            newArray[j + 1] = nums[i + n]
+            j += 2
+        }
+        return newArray
+    }
+}
