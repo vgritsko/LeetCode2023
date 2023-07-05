e5a24e7490f641a1f795a56ee6c92fdae06b0cef
Vadim Gritsko <vadim.gritsko@gmail.com>
2023-07-05 14:24:52 +0700

    35. Search Insert Position https://leetcode.com/problems/search-insert-position/

---
+++ src/main/kotlin/easy/SearchInsertPosition.kt
@@ -0,0 +1,20 @@
+package easy
+
+//https://leetcode.com/problems/search-insert-position/
+class SearchInsertPosition {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var low = 0
+        var high = nums.size - 1
+        var mid: Int
+
+        while (low <= high) {
+            mid = low + ((high - low) / 2)
+            when {
+                target > nums[mid] -> low = mid + 1
+                target == nums[mid] -> return mid
+                target < nums[mid] -> high = mid - 1
+            }
+        }
+        return low
+    }
+}
