dbfc4f3c2898e8d45ba44d69b43b552eb0b26644
Vadim Gritsko <vadim.gritsko@gmail.com>
2023-07-12 16:06:23 +0700

    509. Fibonacci Number https://leetcode.com/problems/fibonacci-number/

---
+++ src/main/kotlin/easy/FibonacciNumber.kt
@@ -0,0 +1,14 @@
+package easy
+https://leetcode.com/problems/fibonacci-number/
+class FibonacciNumber {
+    fun fib(n: Int): Int {
+        val vals = IntArray(n + 2)
+        vals[0] = 0
+        vals[1] = 1
+        if (n <= 1) return vals[n]
+        for (i in 2..n) {
+            vals[i] = vals[i-1] + vals[i-2]
+        }
+        return vals[n]
+    }
+}
