e11d04323eefd03e82e4b57d68c96e3cb84226a1
Vadim Gritsko <vadim.gritsko@gmail.com>
2023-08-26 16:16:38 +0700

    2469. Convert the Temperature https://leetcode.com/problems/convert-the-temperature/

---
+++ src/main/kotlin/easy/ConvertTemperature.kt
@@ -0,0 +1,9 @@
+package easy
+
+class ConvertTemperature {
+    fun convertTemperature(celsius: Double): DoubleArray {
+        val kelvin = celsius + 273.15
+        val fahrenheit = celsius * 1.80 + 32.00
+        return doubleArrayOf(kelvin, fahrenheit)
+    }
+}
